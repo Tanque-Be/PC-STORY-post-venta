4421426b717741a366baf564c6eb2ca7551f4134
Tanque <55325778+Tanque-Be@users.noreply.github.com>
2020-07-01 00:11:39 -0400

    Add files via upload

--- index.py
+++ index.py
@@ -14,6 +14,30 @@ def about():
 def login():
     return render_template('login.html')
 
+@app.route('/bboleta')
+def b_boleta():
+    return render_template('menu_principal/busqueda_boleta.html')
+
+@app.route('/bcliente')
+def b_cliente():
+    return render_template('menu_principal/busqueda_usuario.html')
+
+@app.route('/dev-fallas')
+def fallo():
+    return render_template('menu_principal/ingreso_dev_falla.html')
+
+@app.route('/ticket')
+def ticket():
+    return render_template('menu_principal/seguimiento_ticket.html')
+
+@app.route('/informes')
+def informes():
+    return render_template('menu_principal/informes_general.html')
+
+@app.route('/sugerencias')
+def sug_rec():
+    return render_template('menu_principal/sugerencias_reclamos.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
